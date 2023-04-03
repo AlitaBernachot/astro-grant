532d84421df1a18256f631c5d017ef44a5fbdbc7
AlitaBernachot <alita.bernachot@camptocamp.com>
2023-04-03 18:24:20 +0200

    improve UI

---
+++ src/components/HeaderLink.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps<{
+  href?: string
+  title?: string
+  active?: boolean
+}>()
+</script>
+
+<template>
+    <a :href="props.href" 
+      :title="props.title"
+      :class="props.active === true ? 'bg-gray-900' : 'hover:bg-gray-700 hover:text-white'"
+      class="text-white rounded-md px-3 py-2 text-sm font-medium">
+        <slot>A header link...</slot>
+    </a>
+</template>
--- src/components/common/Button.vue
+++ src/components/common/Button.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+const onClick = () => console.log("Clicked on the button")
 </script>
 
 <template>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded uppercase text-sm">
+    <button
+        @click="onClick"
+        class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded uppercase text-sm">
         <slot>A button...</slot>
     </button>
 </template>
